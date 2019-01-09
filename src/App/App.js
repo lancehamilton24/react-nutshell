0b90070729395e4c7ece126bba4a801e11f72a77
Lance Hamilton <lance.hamilton24@gmail.com>
2019-01-08 21:28:27 -0600

    .

--- src/App/App.js
+++ src/App/App.js
@@ -12,6 +12,7 @@ import Home from '../components/pages/Home/Home';
 import Friends from '../components/pages/Friends/Friends';
 import Events from '../components/pages/Events/Events';
 import Weather from '../components/pages/Weather/Weather';
+import Articles from '../components/pages/Articles/Articles';
 import Messages from '../components/pages/Messages/Messages';
 
 import './App.scss';
@@ -34,6 +35,7 @@ const PrivateRoute = ({ component: Component, authed, ...rest }) => {
 class App extends React.Component {
   state = {
     authed: false,
+    pendingUser: true,
   }
 
   componentDidMount() {
@@ -42,10 +44,12 @@ class App extends React.Component {
       if (user) {
         this.setState({
           authed: true,
+          pendingUser: false,
         });
       } else {
         this.setState({
           authed: false,
+          pendingUser: false,
         });
       }
     });
@@ -56,11 +60,15 @@ class App extends React.Component {
   }
 
   render() {
-    const { authed } = this.state;
+    const { authed, pendingUser } = this.state;
     const logoutClickEvent = () => {
       authRequests.logoutUser();
       this.setState({ authed: false });
     };
+
+    if (pendingUser) {
+      return null;
+    }
 
     return (
       <div className="App">
@@ -76,6 +84,7 @@ class App extends React.Component {
                 <PrivateRoute path='/events' component={Events} authed={this.state.authed} />
                 <PrivateRoute path='/messages' component={Messages} authed={this.state.authed} />
                 <PrivateRoute path='/weather' component={Weather} authed={this.state.authed} />
+                <PrivateRoute path='/articles' component={Articles} authed={this.state.authed} />
                 <PublicRoute path='/auth' component={Auth} authed={this.state.authed} />
               </Switch>
             </div>
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,11 +3,59 @@ import './Home.scss';
 // import { homedir } from 'os';
 
 class Home extends React.Component {
+  changeView = (e) => {
+    const view = e.currentTarget.id;
+    this.props.history.push(`/${view}`);
+  }
+
   render() {
     return (
       <div className='Home'>
-        <h2>Home Component</h2>
+      <div className="card-deck">
+        <div className="card border-dark" id="messages" onClick={this.changeView}>
+          <div className="card-body text-center">
+            <h4 className="card-title"><i className="fas fa-comments fa-7x"></i></h4>
+            <h6 className="card-subtitle mb-2 text-muted">Messages</h6>
+            <p className="card-text">Newer better AOL</p>
+          </div>
+        </div>
+        <div className="card border-dark" id="weather">
+          <div className="card-body text-center">
+            <h4 className="card-title"><i className="fas fa-comments fa-7x"></i></h4>
+            <h6 className="card-subtitle mb-2 text-muted">Weather</h6>
+            <p className="card-text">Newer better AOL</p>
+          </div>
+        </div>
+        <div className="card border-dark" id="events">
+          <div className="card-body text-center">
+            <h4 className="card-title"><i className="fas fa-comments fa-7x"></i></h4>
+            <h6 className="card-subtitle mb-2 text-muted">Events</h6>
+            <p className="card-text">Newer better AOL</p>
+          </div>
+        </div>
+        <div className="card border-dark bg-warning">
+            <div className="card-body text-center">
+              <h4 className="card-title"><i className="fas fa-tree fa-7x"></i></h4>
+              <h6 className="card-subtitle mb-2 text-muted">...</h6>
+              <p className="card-text">Welcome to Nutshell</p>
+            </div>
+          </div>
+        <div className="card border-dark" id="friends">
+          <div className="card-body text-center">
+            <h4 className="card-title"><i className="fas fa-comments fa-7x"></i></h4>
+            <h6 className="card-subtitle mb-2 text-muted">Friends</h6>
+            <p className="card-text">Newer better AOL</p>
+          </div>
+        </div>
+        <div className="card border-dark" id="articles">
+          <div className="card-body text-center">
+            <h4 className="card-title"><i className="fas fa-comments fa-7x"></i></h4>
+            <h6 className="card-subtitle mb-2 text-muted">Articles</h6>
+            <p className="card-text">Newer better AOL</p>
+          </div>
+        </div>
       </div>
+    </div>
     );
   }
 }
