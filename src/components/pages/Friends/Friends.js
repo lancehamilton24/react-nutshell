c22bc9787c6998170808f64f4e4bd6ea37b68d62
Lance Hamilton <lance.hamilton24@gmail.com>
2019-01-08 20:31:21 -0600

    completed routing

---
+++ src/components/pages/Friends/Friends.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './Home.scss';
+// import { homedir } from 'os';
+
+class Friends extends React.Component {
+  render() {
+    return (
+      <div className='Home'>
+        <h2>Friends Component</h2>
+      </div>
+    );
+  }
+}
+
+export default Friends;
---
+++ src/components/pages/Weather/Weather.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './Home.scss';
+// import { homedir } from 'os';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className='Home'>
+        <h2>Weather Component</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
