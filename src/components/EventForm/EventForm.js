449123f8e69c5decfd6d1535562eea7b66d3885c
Lance Hamilton <lance.hamilton24@gmail.com>
2019-01-14 19:22:59 -0600

    wip

--- src/components/EventForm/EventForm.js
+++ src/components/EventForm/EventForm.js
@@ -2,19 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './EventForm.scss';
 import authRequests from '../../helpers/data/authRequests';
-import eventRequests from '../../helpers/data/eventRequests';
+// import eventRequests from '../../helpers/data/eventRequests';
 
 const defaultEvent = {
   event: '',
   startDate: 0,
   location: '',
+  uid: '',
 };
 
 class EventForm extends React.Component {
   static propTypes = {
     onSubmit: PropTypes.func,
-    isEditing: PropTypes.bool,
-    editId: PropTypes.string,
   }
 
   state = {
@@ -30,13 +29,13 @@ class EventForm extends React.Component {
 
   formFieldNumberState = (name, e) => {
     const tempEvent = { ...this.state.newEvent };
-    tempEvent[name] = e.target.value * 1;
+    tempEvent[name] = e.target.value;
     this.setState({ newEvent: tempEvent });
   }
 
   eventChange = e => this.formFieldStringState('event', e);
 
-  startDateChange = e => this.formFieldNumberState('startDate', e);
+  startDateChange = e => this.formFieldStringState('startDate', e);
 
   locationChange = e => this.formFieldStringState('location', e);
 
@@ -49,131 +48,64 @@ class EventForm extends React.Component {
     this.setState({ newEvent: defaultEvent });
   }
 
-  componentDidUpdate(prevProps) {
-    const { isEditing, editId } = this.props;
-    if (prevProps !== this.props && isEditing) {
-      eventRequests.getSingleEvent(editId)
-        .then((event) => {
-          this.setState({ newListing: listing.data });
-        })
-        .catch(err => console.error('error with getSingleListing', err));
-    }
-  }
+  // componentDidUpdate(prevProps) {
+  //   const { isEditing, editId } = this.props;
+  //   if (prevProps !== this.props && isEditing) {
+  //     eventRequests.getAllEvents(editId)
+  //       .then((event) => {
+  //         this.setState({ newEvent: event.data });
+  //       })
+  //       .catch(err => console.error('error with getSingleListing', err));
+  //   }
+  // }
 
   render() {
-    const { newListing } = this.state;
-    const { isEditing } = this.props;
-    const title = () => {
-      if (isEditing) {
-        return <h2>Edit Listing:</h2>;
-      }
-      return <h2>Add New Listing:</h2>;
-    };
+    const { newEvent } = this.state;
     return (
-      <div className="listing-form col">
-        {title()}
+      <div className="event-form col">
+        <h2>New Event:</h2>
         <form onSubmit={this.formSubmit}>
           <div className="form-group">
-            <label htmlFor="address">Address:</label>
+            <label htmlFor="event">Event Name:</label>
             <input
               type="text"
               className="form-control"
               id="address"
               aria-describedby="addressHelp"
               placeholder="123 Main Street Nashville, TN 37209"
-              value={newListing.address}
-              onChange={this.addressChange}
+              value={newEvent.event}
+              onChange={this.eventChange}
             />
           </div>
           <div className="form-group">
-            <label htmlFor="imageUrl">Image Url:</label>
+            <label htmlFor="imageUrl">Start Date:</label>
             <input
               type="text"
               className="form-control"
               id="imageUrl"
               aria-describedby="imageUrlHelp"
               placeholder="www.google.com"
-              value={newListing.imageUrl}
-              onChange={this.imageUrlChange}
+              value={newEvent.startDate}
+              onChange={this.startDateChange}
             />
           </div>
           <div className="form-group">
-            <label htmlFor="squareFootage">Square Footage:</label>
+            <label htmlFor="squareFootage">Location:</label>
             <input
-              type="number"
+              type="text"
               className="form-control"
               id="squareFootage"
               aria-describedby="squareFootageHelp"
               placeholder="1234"
-              value={newListing.squareFootage}
-              onChange={this.squareFootageChange}
+              value={newEvent.location}
+              onChange={this.locationChange}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="numBeds">Number of Bedrooms:</label>
-            <input
-              type="number"
-              className="form-control"
-              id="numBeds"
-              aria-describedby="numBedsHelp"
-              placeholder="4"
-              value={newListing.numBeds}
-              onChange={this.numBedsChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="numBaths">Number of Bathrooms:</label>
-            <input
-              type="number"
-              className="form-control"
-              id="numBaths"
-              aria-describedby="numBathsHelp"
-              placeholder="2"
-              value={newListing.numBaths}
-              onChange={this.numBathsChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="price">Price:</label>
-            <input
-              type="number"
-              className="form-control"
-              id="price"
-              aria-describedby="priceHelp"
-              placeholder="123345345"
-              value={newListing.price}
-              onChange={this.priceChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="heating">Heating:</label>
-            <input
-              type="text"
-              className="form-control"
-              id="heating"
-              aria-describedby="heatingHelp"
-              placeholder="Fireplace"
-              value={newListing.heating}
-              onChange={this.heatingChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="cooling">Cooling:</label>
-            <input
-              type="text"
-              className="form-control"
-              id="cooling"
-              aria-describedby="coolingHelp"
-              placeholder="AC"
-              value={newListing.cooling}
-              onChange={this.coolingChange}
-            />
-          </div>
-          <button className="btn btn-danger">Save Listing</button>
+          <button className="btn btn-danger">Save Event</button>
         </form>
       </div>
     );
   }
 }
 
-export default ListingForm;
+export default EventForm;
--- src/components/pages/Events/Events.js
+++ src/components/pages/Events/Events.js
@@ -2,11 +2,14 @@ import React from 'react';
 import EventItem from '../../EventItem/EventItem';
 import authRequests from '../../../helpers/data/authRequests';
 import smashRequests from '../../../helpers/data/smashRequests';
-
+import eventRequests from '../../../helpers/data/eventRequests';
+import EventForm from '../../EventForm/EventForm';
 
 class Events extends React.Component {
   state = {
     events: [],
+    isEditing: false,
+    editId: '-1',
   }
 
   componentDidMount() {
@@ -16,6 +19,29 @@ class Events extends React.Component {
         this.setState({ events });
       })
       .catch(err => console.error('error with listing GET', err));
+  }
+
+  formSubmitEvent = (newEvent) => {
+    const { isEditing, editId } = this.state;
+    if (isEditing) {
+      eventRequests.putRequest(editId, newEvent)
+        .then(() => {
+          eventRequests.getRequest()
+            .then((events) => {
+              this.setState({ events, isEditing: false, editId: '-1' });
+            });
+        })
+        .catch(err => console.error('error with listings post', err));
+    } else {
+      eventRequests.postRequest(newEvent)
+        .then(() => {
+          eventRequests.getRequest()
+            .then((events) => {
+              this.setState({ events });
+            });
+        })
+        .catch(err => console.error('error with listings post', err));
+    }
   }
 
   render() {
@@ -34,6 +60,9 @@ class Events extends React.Component {
           <div class="event-container">
             <div class="event-cards">{eventsItemComponents}</div>
       </div>
+      <div className="row">
+          <EventForm onSubmit={this.formSubmitEvent} />
+        </div>
     </div>
     );
   }
